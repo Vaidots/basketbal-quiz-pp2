bdba9e6b98b1f0f90ad152cf57a75b248fca69e0
Vaidots <Vaidots30@gmail.com>
2022-06-08 20:48:09 +0000

    Add scipt leaderboard.js to leaderboard html page

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -4,8 +4,18 @@ displayLeaderboard = () => {
 
     leaderboard.sort((a,b) => {
         a = JSON.parse(a)
-        b =JSON.parse(b)
+        b = JSON.parse(b)
 
         return b.score - a.score;
     });
+
+    leaderboard.forEach(function(value, index) {
+        let container = document.getElementById("leaderboard")
+        value = JSON.parse(value);
+        container.innerHTML += `
+        <li>Name: ${value.name}, Score: ${value.score}</li>
+        `
+    })
 }
+
+displayLeaderboard()
