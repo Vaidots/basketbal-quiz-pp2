852b30e7f495a147c783e9a30fbb63e78b08a1a6
Vaidots <Vaidots30@gmail.com>
2022-06-08 18:45:24 +0000

    Add changes to functions adding const and fixed a spelling in game.html page

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,180 +12,180 @@ let availableQuestions = [];
 
 let questions = [
     {
-     question: {
-         id: 1,
-         question: "Who invented the game in 1891?",
-         answers: [{
-             answer: "James Naismith",
-             true: 1
-         },
-         {
-             answer: "Michael Jordan",
-             true: 0
-         },
-         {
-             answer: "James Sherman",
-             true: 0
-         },
-         {
-             answer: "John Edwin",
-             true: 0
-         }
+      question: {
+        id: 1,
+        question: 'Who invented the game in 1891?',
+        answers: [
+          {
+            answer: "James Naismith",
+            true: 1
+          },
+          {
+            answer: "Michael Jordan",
+            true: 0
+          },
+          {
+            answer: "James Sherman",
+            true: 0
+          },
+          {
+            answer: "John Edwin",
+            true: 0
+          }
         ],
-     }   
+      }
     },
-
     {
-        question: {
-            id: 2,
-            question: "How many players in total are on a basketball court at the same time?",
-            answers: [{
-                answer: "12",
-                true: 0
-            },
-            {
-                answer: "5",
-                true: 0
-            },
-            {
-                answer: "10",
-                true: 1
-            },
-            {
-                answer: "8",
-                true: 0
-            }
-           ],
-        }   
-       },
-
-       {
-        question: {
-            id: 3,
-            question: "Which small European country, with a population of under 3 million, is a traditional force of the sport in Europe?",
-            answers: [{
-                answer: "Latvia",
-                true: 0
-            },
-            {
-                answer: "Estonia",
-                true: 0
-            },
-            {
-                answer: "Lithuania",
-                true: 1
-            },
-            {
-                answer: "Poland",
-                true: 0
-            }
-           ],
-        }   
-       },
-
-       {
-        question: {
-            id: 4,
-            question: "Michael Jordans leaping ability earned him which nickname?",
-            answers: [{
-                answer: "Air Jordan",
-                true: 1
-            },
-            {
-                answer: "Air Ball",
-                true: 0
-            },
-            {
-                answer: "Flying Jordan",
-                true: 0
-            },
-            {
-                answer: "Unstopable Jordan",
-                true: 0
-            }
-           ],
-        }   
-       },
-
-       {
-        question: {
-            id: 5,
-            question: "Who was known for his fierce defensive and rebounding abilities, and was nicknamed the worm??",
-            answers: [{
-                answer: "Lebron James",
-                true: 0
-            },
-            {
-                answer: "Scotty Pippen",
-                true: 0
-            },
-            {
-                answer: "James Sherman",
-                true: 0
-            },
-            {
-                answer: "Dennis Rodman",
-                true: 1
-            }
-           ],
-        }   
-       },
-
-       {
-        question: {
-            id: 6,
-            question: "The United States mens Olympic basketball team, nicknamed the 'Dream Team', competed in which year at the Olympics?",
-            answers: [{
-                answer: "1992",
-                true: 0
-            },
-            {
-                answer: "1988",
-                true: 0
-            },
-            {
-                answer: "1996",
-                true: 0
-            },
-            {
-                answer: "2000",
-                true: 1
-            }
-           ],
-        }   
-       },
-]
+      question: {
+        id: 2,
+        question: 'How many players in total are on a basketball court at the same time?',
+        answers: [
+          {
+            answer: "12",
+            true: 0
+          },
+          {
+            answer: "5",
+            true: 0
+          },
+          {
+            answer: "10",
+            true: 1
+          },
+          {
+            answer: "8",
+            true: 0
+          }
+        ]
+      }
+    },
+    {
+      question: {
+        id: 3,
+        question: 'Which small European country, with a population of under 3 million, is a traditional force of the sport in Europe?',
+        answers: [
+          {
+            answer: "Latvia",
+            true: 0
+          },
+          {
+            answer: "Estonia",
+            true: 0
+          },
+          {
+            answer: "Lithuania",
+            true: 1
+          },
+          {
+            answer: "Poland",
+            true: 0
+          }
+        ]
+      }
+    },
+    {
+      question: {
+        id: 4,
+        question: 'Michael Jordans leaping ability earned him which nickname?',
+        answers: [
+          {
+            answer: "Air Jordan",
+            true: 1
+          },
+          {
+            answer: "Air ball",
+            true: 0
+          },
+          {
+            answer: "Flying Jordan",
+            true: 0
+          },
+          {
+            answer: "Unstopable Jordan",
+            true: 0
+          }
+        ]
+      }
+    },
+    {
+      question: {
+        id: 5,
+        question: 'Who was known for his fierce defensive and rebounding abilities, and was nicknamed the worm?',
+        answers: [
+          {
+            answer: "LeBron James",
+            true: 0
+          },
+          {
+            answer: "Scotty Pipen",
+            true: 0
+          },
+          {
+            answer: "James Sherman",
+            true: 0
+          },
+          {
+            answer: "Dennis Rodman",
+            true: 1
+          }
+        ]
+      }
+    },
+    {
+      question: {
+        id: 6,
+        question: 'The United States mens Olympic basketball team, nicknamed the "Dream Team", competed in which year at the Olympics?',
+        answers: [
+          {
+            answer: "1992",
+            true: 0
+          },
+          {
+            answer: "1988",
+            true: 0
+          },
+          {
+            answer: "1996",
+            true: 0
+          },
+          {
+            answer: "2000",
+            true: 1
+          }
+        ]
+      }
+    },
+  ]
 
 const SCORE_POINTS = 20
 
 const MAX_QUESTIONS = 6
 
-startGame = () => {
+const startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     newQuestion()
 }
 
-newQuestion = () => {
-    let answerDiv = document.getElementById("answers");
-    let infoMessage = document.getElementById("infoMessage");
-    answerDiv.innerHTML = "";
-    infoMessage.innerHTML = "";
+const newQuestion = () => {
+    let answersDiv = document.getElementById("answers")
+  let infoMessage = document.getElementById("infoMessage");
+  answersDiv.innerHTML = "";
+  infoMessage.innerHTML = "";
 
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem("mostRecentScore", score);
-        return window.location.assign("endgame.html");
+  if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+    localStorage.setItem('mostRecentScore', score)
+    return window.location.assign('endgame.html')
     }
 
     questionCounter++
     progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`;
     progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-
-    question.innerHTML = currentQuestion.question.question
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question.question
 
     currentQuestion.question.answers.forEach(function(value,index){
         let choiceContainer = document.createElement("div");
